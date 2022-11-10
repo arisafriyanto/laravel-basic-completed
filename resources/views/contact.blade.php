3efa1fadc56ae85f247a979ec79b6d8d3add1b5f
afriyan <arisafriyanto1933@gmail.com>
2022-11-11 03:58:12 +0700

    setup component layouts

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
-</head>
-
-<body>
-    
-    <ul class="navbar">
-        <li><a href="/">Home</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/posts/first-post">Blog</a></li>
-    </ul>
-
-    Contact Page
-
-    <script src="{{ asset("js/app.js") }}"></script>
-</body>
-
-</html>
+<x-app-layout title="Contact Page">
+    <h1>Contact Page</h1>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", fn () => view("home")); // 7.4
-// Route::get("/", fn () => dd(asset("css/app.css"))); // 7.4
 
 Route::view("/contact", "contact");
-
-Route::get("posts/first-post", function () {
-    return view("posts.first-post");
-});
+Route::view("about", "about");
+Route::view("profile", "profile");
